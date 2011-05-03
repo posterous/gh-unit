24dfe8b95af4b9e884de37dbb82ba5d3f482df6b
Adam Huda <huda@posterous-inc.com>
2011-05-03 13:33:56 -0700

    change the default app delegate

--- Project-IPhone/GHUnitIOSTestMain.m
+++ Project-IPhone/GHUnitIOSTestMain.m
@@ -75,7 +75,7 @@ int main(int argc, char *argv[]) {
   if (getenv("GHUNIT_CLI")) {
     retVal = [GHTestRunner run];
   } else {
-    retVal = UIApplicationMain(argc, argv, nil, @"GHUnitIPhoneAppDelegate");
+    retVal = UIApplicationMain(argc, argv, nil, @"AppDelegateTest");
   }
   [pool release];
   return retVal;
